9e7791687f4c0fb58f07512f4e3be310cec94f35
Andrzej Laczak <laczakan@yahoo.co.uk>
2020-12-18 15:34:50 +0000

    Added info in index

--- App/Views/index/index.php
+++ App/Views/index/index.php
@@ -3,3 +3,4 @@
 ?>
 <h1 class="mt-5">My first blog .v5 </h1>
 <p class="lead">My first portfolio <code>using clear php</code> with use of  <code> Bootstrap</code>.</p>
+<h3 class="text-center">1-31 July blog project</h3>
